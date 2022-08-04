96b7f83b6297bcd4881b3cebbc7f2cbe28d12e45
Alex1703 <p.a.rodriguez1102@gmail.com>
2022-08-04 18:21:34 -0500

    Creations module Client

---
+++ ReferenceManager.App/Controllers/RefArrendadorViviendumsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ReferenceManager.App.Models;
+
+namespace ReferenceManager.App.Controllers
+{
+    public class RefArrendadorViviendumsController : Controller
+    {
+        private readonly DBReferenciasContext _context;
+
+        public RefArrendadorViviendumsController(DBReferenciasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RefArrendadorViviendums
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.RefArrendadorVivienda.ToListAsync());
+        }
+
+        // GET: RefArrendadorViviendums/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.RefArrendadorVivienda == null)
+            {
+                return NotFound();
+            }
+
+            var refArrendadorViviendum = await _context.RefArrendadorVivienda
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (refArrendadorViviendum == null)
+            {
+                return NotFound();
+            }
+
+            return View(refArrendadorViviendum);
+        }
+
+        // GET: RefArrendadorViviendums/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RefArrendadorViviendums/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,QueArrienda,QuienVive,EstadoCivil,NombreConyuge,Actividad,TiempoArriendo,DireccionVivienda,CanonArriendo,IncluyeServicios,PuntualResponsable,Concepto")] RefArrendadorViviendum refArrendadorViviendum)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(refArrendadorViviendum);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(refArrendadorViviendum);
+        }
+
+        // GET: RefArrendadorViviendums/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.RefArrendadorVivienda == null)
+            {
+                return NotFound();
+            }
+
+            var refArrendadorViviendum = await _context.RefArrendadorVivienda.FindAsync(id);
+            if (refArrendadorViviendum == null)
+            {
+                return NotFound();
+            }
+            return View(refArrendadorViviendum);
+        }
+
+        // POST: RefArrendadorViviendums/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,QueArrienda,QuienVive,EstadoCivil,NombreConyuge,Actividad,TiempoArriendo,DireccionVivienda,CanonArriendo,IncluyeServicios,PuntualResponsable,Concepto")] RefArrendadorViviendum refArrendadorViviendum)
+        {
+            if (id != refArrendadorViviendum.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(refArrendadorViviendum);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RefArrendadorViviendumExists(refArrendadorViviendum.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(refArrendadorViviendum);
+        }
+
+        // GET: RefArrendadorViviendums/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.RefArrendadorVivienda == null)
+            {
+                return NotFound();
+            }
+
+            var refArrendadorViviendum = await _context.RefArrendadorVivienda
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (refArrendadorViviendum == null)
+            {
+                return NotFound();
+            }
+
+            return View(refArrendadorViviendum);
+        }
+
+        // POST: RefArrendadorViviendums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.RefArrendadorVivienda == null)
+            {
+                return Problem("Entity set 'DBReferenciasContext.RefArrendadorVivienda'  is null.");
+            }
+            var refArrendadorViviendum = await _context.RefArrendadorVivienda.FindAsync(id);
+            if (refArrendadorViviendum != null)
+            {
+                _context.RefArrendadorVivienda.Remove(refArrendadorViviendum);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RefArrendadorViviendumExists(int id)
+        {
+          return _context.RefArrendadorVivienda.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ReferenceManager.App/Controllers/RefFamiliarsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ReferenceManager.App.Models;
+
+namespace ReferenceManager.App.Controllers
+{
+    public class RefFamiliarsController : Controller
+    {
+        private readonly DBReferenciasContext _context;
+
+        public RefFamiliarsController(DBReferenciasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RefFamiliars
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.RefFamiliars.ToListAsync());
+        }
+
+        // GET: RefFamiliars/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.RefFamiliars == null)
+            {
+                return NotFound();
+            }
+
+            var refFamiliar = await _context.RefFamiliars
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (refFamiliar == null)
+            {
+                return NotFound();
+            }
+
+            return View(refFamiliar);
+        }
+
+        // GET: RefFamiliars/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RefFamiliars/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ConfirmacionNombre,Parentezco,EstadoCivil,NombreConyuge,CantidadHijos,QuienVive,Actividad,TiempoNegocio,BarrioNegocio,CantidadEmpleados,DireccionTelefono,Concepto")] RefFamiliar refFamiliar)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(refFamiliar);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(refFamiliar);
+        }
+
+        // GET: RefFamiliars/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.RefFamiliars == null)
+            {
+                return NotFound();
+            }
+
+            var refFamiliar = await _context.RefFamiliars.FindAsync(id);
+            if (refFamiliar == null)
+            {
+                return NotFound();
+            }
+            return View(refFamiliar);
+        }
+
+        // POST: RefFamiliars/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ConfirmacionNombre,Parentezco,EstadoCivil,NombreConyuge,CantidadHijos,QuienVive,Actividad,TiempoNegocio,BarrioNegocio,CantidadEmpleados,DireccionTelefono,Concepto")] RefFamiliar refFamiliar)
+        {
+            if (id != refFamiliar.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(refFamiliar);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RefFamiliarExists(refFamiliar.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(refFamiliar);
+        }
+
+        // GET: RefFamiliars/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.RefFamiliars == null)
+            {
+                return NotFound();
+            }
+
+            var refFamiliar = await _context.RefFamiliars
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (refFamiliar == null)
+            {
+                return NotFound();
+            }
+
+            return View(refFamiliar);
+        }
+
+        // POST: RefFamiliars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.RefFamiliars == null)
+            {
+                return Problem("Entity set 'DBReferenciasContext.RefFamiliars'  is null.");
+            }
+            var refFamiliar = await _context.RefFamiliars.FindAsync(id);
+            if (refFamiliar != null)
+            {
+                _context.RefFamiliars.Remove(refFamiliar);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RefFamiliarExists(int id)
+        {
+          return _context.RefFamiliars.Any(e => e.Id == id);
+        }
+    }
+}
